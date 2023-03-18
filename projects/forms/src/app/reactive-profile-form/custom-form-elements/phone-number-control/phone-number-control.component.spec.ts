c05534674fdbbc99d4d91b562665a6f7a6550302
Thomas Thomsen <thomas.thomsen@xpo.com>
2023-03-18 00:11:26 -0300

    harness tests

--- projects/forms/src/app/reactive-profile-form/custom-form-elements/phone-number-control/phone-number-control.component.spec.ts
+++ projects/forms/src/app/reactive-profile-form/custom-form-elements/phone-number-control/phone-number-control.component.spec.ts
@@ -1,10 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { PhoneNumberControlComponent } from './phone-number-control.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { runOnPushChangeDetection } from './../../../testing/on-push-change-detection.helper';
 
 describe('PhoneNumberControlComponent', () => {
   let component: PhoneNumberControlComponent;
   let fixture: ComponentFixture<PhoneNumberControlComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +18,24 @@ describe('PhoneNumberControlComponent', () => {
 
     fixture = TestBed.createComponent(PhoneNumberControlComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should show inputs', async () => {
+    component.hint = 'test';
+    component.label = 'test';
+    runOnPushChangeDetection(fixture);
+
+    const formField = await loader.getHarness(MatFormFieldHarness);
+    const hint = await formField.getTextHints();
+    const label = await formField.getLabel();
+
+    expect(hint).toEqual(['test']);
+    expect(label).toEqual('test');
   });
 });
