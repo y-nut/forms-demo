f36e4e706f0791acc46a960db4ebaf450d3e31ab
Thomas Thomsen <thomas.thomsen@xpo.com>
2023-03-24 18:07:51 -0300

    added pwa

--- projects/forms/src/app/app.module.ts
+++ projects/forms/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from './translation/translate.module';
 import { Language } from './shared/enums/language.enum';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +19,12 @@ import { HttpClientModule } from '@angular/common/http';
       currentLanguage: Language.ES,
       defaultLanguage: Language.ES,
       // initialLoadLanguages: [Language.ES, Language.EN],
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   bootstrap: [AppComponent],
