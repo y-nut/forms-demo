7e44096aaa7c32c2a48d234b240846c713aedf57
Thomas Thomsen <thomas.thomsen@xpo.com>
2023-07-22 12:12:07 -0400

    better

--- projects/signals/src/app/product-list/qty/qty.component.ts
+++ projects/signals/src/app/product-list/qty/qty.component.ts
@@ -1,12 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  Injector,
-  Input,
-  computed,
-  inject,
-  signal,
-} from '@angular/core';
+import { Component, Injector, Input, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CheckoutService } from '../../services/checkout.service';
@@ -20,28 +13,8 @@ import { CheckoutService } from '../../services/checkout.service';
 })
 export class QtyComponent {
   private injector = inject(Injector);
-  productIdSignal = signal<number | null>(null);
-
-  basketItem = computed(() => {
-    const productId = this.productIdSignal();
-    const basket = this.checkoutService.basket;
-    if (typeof productId === 'number' && basket.has(productId)) {
-      const item = basket.get(productId);
-      if (item) {
-        return item();
-      }
-    }
-    return null;
-  });
-
-  @Input()
-  set productId(productId: number) {
-    this.productIdSignal.set(productId);
-  }
-
+  checkoutService = this.injector.get(CheckoutService);
   basket = this.checkoutService.basket;
 
-  get checkoutService() {
-    return this.injector.get(CheckoutService);
-  }
+  @Input() productId: number | undefined;
 }
